5255e641f3a311feda2f5933ac438b9ac5b6e2eb
emerson <69053988+Jed2020@users.noreply.github.com>
2021-12-19 12:11:46 -0300

    alterações em style.css script.js e index.html

--- script.js
+++ script.js
@@ -1,107 +1,88 @@
-let order = [];
-let clickedOrder = [];
-let score = 0;
+const _data = {
+	gameOn: false,
+	timeout: undefined,
+	sounds: [],
 
-//0 - verde
-//1 - vermelho
-//2 - amarelo
-//3 - azul
+	strict: false,
+	playerCanPlay: false,
+	score: 0,
+	gameSequence: [],
+	playerSequence: []
+};
 
-const blue = document.querySelector('.blue');
-const red = document.querySelector('.red');
-const green = document.querySelector('.green');
-const yellow = document.querySelector('.yellow');
-
-//cria ordem aletoria de cores
-let shuffleOrder = () => {
-    let colorOrder = Math.floor(Math.random() * 4);
-    order[order.length] = colorOrder;
-    clickedOrder = [];
-
-    for(let i in order) {
-        let elementColor = createColorElement(order[i]);
-        lightColor(elementColor, Number(i) + 1);
-    }
+const _gui = {
+	counter: document.querySelector(".gui__counter"),
+	switch: document.querySelector(".gui__btn-switch"),
+	led: document.querySelector(".gui__led"),
+	strict: document.querySelector(".gui__btn--strict"),
+	start: document.querySelector(".gui__btn--start"),
+	pads: document.querySelectorAll(".game__pad")
 }
 
-//acende a proxima cor
-let lightColor = (element, number) => {
-    number = number * 500;
-    setTimeout(() => {
-        element.classList.add('selected');
-    }, number - 250);
-    setTimeout(() => {
-        element.classList.remove('selected');
-    });
+const _soundUrls = [
+	"audio/simonSound1.mp3",
+	"audio/simonSound2.mp3",
+	"audio/simonSound3.mp3",
+	"audio/simonSound4.mp3"
+];
+
+_soundUrls.forEach(sndPath => {
+	const audio = new Audio(sndPath);
+	_data.sounds.push(audio);
+});
+
+_gui.switch.addEventListener("click", () => {
+
+});
+
+_gui.strict.addEventListener("click", () => {
+
+});
+
+_gui.start.addEventListener("click", () => {
+
+});
+
+const padListener = (e) => {
+
 }
 
-//checa se os botoes clicados são os mesmos da ordem gerada no jogo
-let checkOrder = () => {
-    for(let i in clickedOrder) {
-        if(clickedOrder[i] != order[i]) {
-            gameOver();
-            break;
-        }
-    }
-    if(clickedOrder.length == order.length) {
-        alert(`Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`);
-        nextLevel();
-    }
+_gui.pads.forEach(pad => {
+	pad.addEventListener("click", padListener);
+});
+
+const startGame = () => {
+
 }
 
-//funcao para o clique do usuario
-let click = (color) => {
-    clickedOrder[clickedOrder.length] = color;
-    createColorElement(color).classList.add('selected');
+const setScore = () => {
 
-    setTimeout(() => {
-        createColorElement(color).classList.remove('selected');
-        checkOrder();
-    },250);
 }
 
-//funcao que retorna a cor
-let createColorElement = (color) => {
-    if(color == 0) {
-        return green;
-    } else if(color == 1) {
-        return red;
-    } else if (color == 2) {
-        return yellow;
-    } else if (color == 3) {
-        return blue;
-    }
+const newColor = () => {
+
 }
 
-//funcao para proximo nivel do jogo
-let nextLevel = () => {
-    score++;
-    shuffleOrder();
+const playSequence = () => {
+
 }
 
-//funcao para game over
-let gameOver = () => {
-    alert(`Pontuação: ${score}!\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo`);
-    order = [];
-    clickedOrder = [];
+const blink = (text, callback) => {
 
-    playGame();
 }
 
-//funcao de inicio do jogo
-let playGame = () => {
-    alert('Bem vindo ao Gênesis! Iniciando novo jogo!');
-    score = 0;
+const waitForPlayerClick = () => {
 
-    nextLevel();
 }
 
-//eventos de clique para as cores
-green.onclick = () => click(0);
-red.onclick = () => click(1);
-yellow.onclick = () => click(2);
-blue.onclick = () => click(3);
+const resetOrPlayAgain = () => {
 
+}
 
-//inicio do jogo
-playGame();
+const changePadCursor = (cursorType) => {
+
+}
+
+const disablePads = () => {
+
+}
